0f88d546ff85cc3ad3d8baa411b7bc561ff050df
Dylan Wang <wyc6990906@hotmail.com>
2022-08-02 16:31:37 -0600

    cypress home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,7 @@
+it("There is products on the page", () => {
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.get(".products article").should("have.length", 2);
+});
